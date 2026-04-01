fdd66fd1761fbd2ff91a68547d1ffd6c9479d6ef
BinTr <bintran2008a@gmail.com>
2026-04-01 12:32:30 +0700

    fix: remove incompatible BatchSize annotations causing Hibernate startup failure

--- backend/src/main/java/com/pbl3/timtro/room/entity/Room.java
+++ backend/src/main/java/com/pbl3/timtro/room/entity/Room.java
@@ -3,7 +3,6 @@ import com.pbl3.timtro.room.enums.RoomStatus;
 import com.pbl3.timtro.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -52,7 +51,6 @@ public class Room {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id")
-    @BatchSize(size = 50)
     private User owner;
     @Builder.Default
     @ManyToMany
@@ -61,12 +59,10 @@ public class Room {
             joinColumns = @JoinColumn(name = "room_id"),
             inverseJoinColumns = @JoinColumn(name = "amenity_id")
     )
-            @BatchSize(size = 50)
     private Set<Amenity> amenities = new HashSet<>();
 
     @Builder.Default
     @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
-            @BatchSize(size = 50)
     private List<RoomImage> images = new ArrayList<>();
 
     @CreationTimestamp
